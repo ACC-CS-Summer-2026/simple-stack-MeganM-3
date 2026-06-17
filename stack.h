d73ecc690e5b236a28dd6cefb836df98239a17c7
megan <megan.m3113@gmail.com>
2026-06-17 00:32:15 +0000

    add stack methods

--- stack.h
+++ stack.h
@@ -33,6 +33,10 @@ protected: // we use protected here to allow access to derived classes
     /**********************
     Methods
     ***********************/
+    int pop(); // or bool pop(int*); // remove and return the top value
+    int peek(); // or bool peek(int*); // return the top value
+    bool push(int); // insert to the top of the stack
+    bool isEmpty(); // test for empty (i.e., top < 0)
 
     /**********************
     Attributes
