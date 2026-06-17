2bcb11d5b6fe0d6bc327b577c29fcd7b53cd3cc3
megan <megan.m3113@gmail.com>
2026-06-17 00:23:30 +0000

    add directive, base main class to return 0

--- main.cpp
+++ main.cpp
@@ -3,3 +3,18 @@
  * Assignment: Assignment 4
  * Purpose: 
  ************************************************* */
+ 
+ #include "main.h"
+ 
+ int main() {
+     /* *************************************************
+    This function is the application driver. ---TBD---
+
+    @param na : ---TBD---
+    @return (int) : ---TBD---
+    @exception na : ---TBD---
+    @note ---TBD---
+    * ************************************************* */
+    
+    return 0;
+}
