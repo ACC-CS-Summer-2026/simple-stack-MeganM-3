fe1ea3aecf75c6d3b0d5481efcda391284aaaf0e
megan <megan.m3113@gmail.com>
2026-06-17 00:25:16 +0000

    add directive

--- stack.cpp
+++ stack.cpp
@@ -1,5 +1,7 @@
 /* *************************************************
  * Name: Megan Triesch
  * Assignment: Assignment 4
- * Purpose: 
+ * Purpose: Create and define 'stack' data structure class.
  ************************************************* */
+  
+#include "stack.h"
