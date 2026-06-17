6f0f2a04900dac12c16e44ce1636e65a20ae7110
megan <megan.m3113@gmail.com>
2026-06-17 00:24:47 +0000

    add directives, base empty stack class, and section headers

--- stack.h
+++ stack.h
@@ -1,5 +1,43 @@
 /* *************************************************
  * Name: Megan Triesch
  * Assignment: Assignment 4
- * Purpose: 
+ * Purpose: Create and define 'stack' data structure class.
  ************************************************* */
+ 
+ #ifndef CLASSES_STACK_H
+ #define CLASSES_STACK_H 
+ 
+ class Stack {
+ 
+    public:
+    /**********************
+    Constructors/Destructor
+    ***********************/
+    
+
+    /**********************
+    Getters/Accessors
+    ***********************/
+    
+
+    /**********************
+    Setters/Mutators
+    ***********************/
+    
+
+    /**********************
+    Printing
+    ***********************/
+
+protected: // we use protected here to allow access to derived classes
+    /**********************
+    Methods
+    ***********************/
+
+    /**********************
+    Attributes
+    ***********************/
+    
+};
+
+#endif //CLASSES_STACK_H
