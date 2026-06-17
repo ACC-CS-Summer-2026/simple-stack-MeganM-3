05525cf5fe6cc76b898c7f09fdc4e0cbec981de0
megan <megan.m3113@gmail.com>
2026-06-17 00:17:03 +0000

    add directives

--- main.h
+++ main.h
@@ -1,5 +1,14 @@
 /* *************************************************
  * Name: Megan Triesch
  * Assignment: Assignment 4
- * Purpose: 
+ * Purpose: Header file for main. All directives for
+ * main go here.
  ************************************************* */
+ 
+#ifndef MAIN_CLASSES_H
+#define MAIN_CLASSES_H
+
+#include "stack.h"
+#include <iostream>
+
+#endif /* MAIN_CLASSES_H */
